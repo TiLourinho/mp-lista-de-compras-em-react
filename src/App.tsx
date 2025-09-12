6889412f727d9a6c5e2288ad8370f55c35c66faf
Tiago Lourinho <lourinho.tiago@gmail.com>
2025-09-12 10:03:21 -0300

    Componentize header

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,14 @@
-import logo from './assets/logo.svg';
-import trash from './assets/trash.svg';
-import todo from './assets/todo.svg';
-import done from './assets/done.svg';
+import Header from "./components/Header";
+
+import trash from "./assets/trash.svg";
+import todo from "./assets/todo.svg";
+import done from "./assets/done.svg";
 
 function App() {
   return (
     <main className="max-w-2xl px-6 py-12 pb-20 mx-auto my-10 bg-white md:my-20 md:px-32 md:rounded-3xl">
-      <header className="text-center">
-        <img src={logo} alt="logotipo" className="mx-auto" />
-        <h1 className="mt-4 text-3xl font-medium font-display">
-          Lista de Compras
-        </h1>
-        <p className="text-sm text-slate-500">
-          Facilite sua ida ao supermercado!
-        </p>
-        <hr className="w-1/3 mx-auto mt-6 mb-8" />
-      </header>
-      <form className="flex gap-2">
-        <div className="flex-shrink">
-          <label htmlFor="name" className="block text-xs text-slate-400">
-            Item
-          </label>
-          <input
-            type="text"
-            id="name"
-            className="block w-full px-3 py-2 border rounded-lg border-slate-300 text-slate-700"
-          />
-        </div>
-        <div className="flex-shrink">
-          <label htmlFor="quantity" className="block text-xs text-slate-400">
-            Quantidade
-          </label>
-          <input
-            type="text"
-            id="quantity"
-            className="block w-full px-3 py-2 border rounded-lg border-slate-300 text-slate-700"
-          />
-        </div>
-        <button className="self-end flex-shrink h-10 px-4 font-extrabold text-white rounded-lg bg-fuchsia-300">
-          +
-        </button>
-      </form>
+      <Header />
+
       <section className="mt-10 space-y-3 ">
         <article className="flex w-full gap-4">
           <img src={todo} alt="#" />
