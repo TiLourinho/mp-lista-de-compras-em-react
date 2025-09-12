c6cb2681b1624913d235331d12c50dab3e303b0b
Tiago Lourinho <lourinho.tiago@gmail.com>
2025-09-12 16:16:43 -0300

    Componentize first section

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import Header from "./components/Header";
 import Form from "./components/Form";
+import ItemsList from "./components/ItemsList";
 
 import trash from "./assets/trash.svg";
-import todo from "./assets/todo.svg";
 import done from "./assets/done.svg";
 
 function App() {
@@ -10,34 +10,7 @@ function App() {
     <main className="max-w-2xl px-6 py-12 pb-20 mx-auto my-10 bg-white md:my-20 md:px-32 md:rounded-3xl">
       <Header />
       <Form />
-      <section className="mt-10 space-y-3 ">
-        <article className="flex w-full gap-4">
-          <img src={todo} alt="#" />
-          <div className="flex-1">
-            <p>Leite</p>
-            <p className="text-sm text-slate-400">3 Caixas</p>
-          </div>
-          <img
-            src={trash}
-            alt="ícone de lixeira"
-            className="justify-self-end"
-          />
-        </article>
-        <hr />
-        <article className="flex w-full gap-4">
-          <img src={todo} alt="#" />
-          <div className="flex-1">
-            <p>Maçã</p>
-            <p className="text-sm text-slate-400">500g</p>
-          </div>
-          <img
-            src={trash}
-            alt="ícone de lixeira"
-            className="justify-self-end"
-          />
-        </article>
-        <hr />
-      </section>
+      <ItemsList />
       <section className="mt-16 space-y-3">
         <h2 className="mb-10 text-3xl text-center font-display">
           Itens já comprados
